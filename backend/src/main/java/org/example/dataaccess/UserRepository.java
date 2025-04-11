2d4e7cc7a299223a793c885f04e78e6e3365256b
parker wallace <wallace.parker@hotmail.com>
2025-04-11 13:46:26 -0230

    removed unused imports

--- backend/src/main/java/org/example/dataaccess/UserRepository.java
+++ backend/src/main/java/org/example/dataaccess/UserRepository.java
@@ -1,15 +1,16 @@
 package org.example.dataaccess;
 
 import org.example.pojos.Core.User;
-import org.example.pojos.Home.Home;
+
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.http.converter.json.GsonBuilderUtils;
+
 
 /**
  * User Repository interface that will be used by String to create a bean that handles all the CRUD operations
  */
 public interface UserRepository extends CrudRepository<User, Long> {
    User getUserById(Integer id);
+   User getUserByEmail(String Email);
 
     // This is where you write code needed beyond the basics
 
