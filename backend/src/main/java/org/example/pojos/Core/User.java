3d31feee580eafbe40fa44eb8c0a0475937f34a3
parker wallace <wallace.parker@hotmail.com>
2025-04-11 12:29:03 -0230

    updated user to represent both customer and Employee for tokenization permission based management

--- backend/src/main/java/org/example/pojos/Core/User.java
+++ backend/src/main/java/org/example/pojos/Core/User.java
@@ -1,9 +1,15 @@
 package org.example.pojos.Core;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+
 
 /**
  * Represents a User Entity with shared attributes between a Customer and Representative
@@ -13,22 +19,24 @@ import jakarta.persistence.Id;
 @Entity
 public class User {
 
+    public enum Role {
+        REPRESENTATIVE,
+        CUSTOMER
+    }
+
     /**
      * The unique identifier for this User.
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
-    /**
-     * This User's name identifier.
-     */
     private String username;
-
-    /**
-     * This User's email.
-     */
     private String email;
+    private LocalDate dateOfBirth;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     /**
      * Default contructor required by JPA.
@@ -40,14 +48,14 @@ public class User {
      * @param username the username for the user.
      * @param email the email contact for the user.
      */
-    public User(String username, String email) {
+    public User(String username, String email, String password) {
         this.email = email;
         this.username = username;
+        this.password = password;
     }
     
-    // *******************
     // GETTERS AND SETTERS
-    // *******************
+
     /**
      * Gets the unique identifier of this User.
      * @return
@@ -72,6 +80,18 @@ public class User {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
     /**
      * Sets this User's email property.
      * @param email The new email for this User.
@@ -86,5 +106,17 @@ public class User {
      */
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
