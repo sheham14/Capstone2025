575e81feec3267b3edc6a3d651523578bc314d04
parker wallace <wallace.parker@hotmail.com>
2025-04-11 13:45:32 -0230

    added login to the path TODO: update this to just be in the usercontroller file

--- backend/src/main/java/org/example/controllers/RESTNouns.java
+++ backend/src/main/java/org/example/controllers/RESTNouns.java
@@ -7,6 +7,7 @@ package org.example.controllers;
 public class RESTNouns {
     // USER MANAGEMENT
     public static final String TOKEN = "/{token}";
+    public static final String LOGIN = "/login";
     public static final String USER = "/users";
     public static final String ID = "/{id}";
     public static final String HOME = "/homes";
