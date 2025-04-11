fccbf1535a42d1d448a1be6f18eca82481fecf07
parker wallace <wallace.parker@hotmail.com>
2025-04-11 13:46:39 -0230

    created the LoginToken core file

---
+++ backend/src/main/java/org/example/pojos/Core/LoginToken.java
@@ -0,0 +1,36 @@
+package org.example.pojos.Core;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+public class LoginToken {
+    @Id
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name= "user_id")
+    private User user;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public LoginToken() {}
+
+    public String getToken() {
+        return this.token;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setTokenOwner (User user) {
+        this.user = user;
+    }
+}
