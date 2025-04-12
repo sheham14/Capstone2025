80930fe48fe15fd40a804ceeb4d15bb4fbc02560
parker wallace <wallace.parker@hotmail.com>
2025-04-12 15:53:19 -0230

    updated to match annotation and sys requirements

--- backend/src/main/java/org/example/pojos/Home/HomeInsurance.java
+++ backend/src/main/java/org/example/pojos/Home/HomeInsurance.java
@@ -3,28 +3,43 @@ package org.example.pojos.Home;
 import java.time.LocalDate;
 
 import org.example.pojos.Core.Policy;
-import org.example.pojos.User.Customer;
+import org.example.pojos.Core.User;
 
+import jakarta.persistence.*;
+
+@Entity
 public class HomeInsurance extends Policy {
-   private Home insuredHome;
-   private boolean hasAutoPolicyDiscount;
 
-   public HomeInsurance(String policyId,
-                       Customer insuredPerson,
+    @Embedded
+    private Home insuredHome;
+  
+    private boolean hasAutoPolicyDiscount;
+  
+    public HomeInsurance() {
+    }
+
+    public HomeInsurance(
+                       User policyOwner,
                        LocalDate startDate,
                        double basePremium,
                        double taxRate,
+                       POLICYVIEWINGTYPE viewingType,
+                       boolean activeStatus,
                        Home insuredHome,
                        boolean hasAutoPolicyDiscount,
                        double totalPremium) {
-       super(policyId, insuredPerson, startDate, basePremium, taxRate);
+       super(policyOwner, startDate, basePremium, taxRate, viewingType, activeStatus);
        this.insuredHome = insuredHome;
        this.hasAutoPolicyDiscount = hasAutoPolicyDiscount;
        setTotalPremium(totalPremium);
    }
 
-   public Home getHome() {
+   public Home getInsuredHome() {
        return this.insuredHome;
+   }
+
+   public void setInsuredHome(Home insuredHome) {
+    this.insuredHome = insuredHome;
    }
 
    public boolean hasAutoPolicyDiscount() {
