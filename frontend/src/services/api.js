b0c8ee9980db88b9107712c372c33394530b4e53
sheham14 <sheham.shahid@gmail.com>
2025-07-22 23:43:39 -0230

    Added env variable to API url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -38,7 +38,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Backend port
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
 });
 
 api.interceptors.request.use(async (config) => {
